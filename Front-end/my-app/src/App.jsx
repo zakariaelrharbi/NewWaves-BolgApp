7aec001f5008675c9aadf85efb84a6d8b4573b0a
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-04-27 13:05:58 +0100

    register page creation

--- Front-end/my-app/src/App.jsx
+++ Front-end/my-app/src/App.jsx
@@ -3,12 +3,14 @@ import Header from './Components/Header'
 import HomePage from './pages/Home/HomePage'
 import { Routes, Route } from 'react-router-dom'
 import ArticleDetailPage from './pages/articleDetail/ArticleDetailPage'
+import Register from './pages/Register/Register'
 const App = () => {
   return (
     <div className="App font-opensans">
       <Routes>
         <Route index path="/" element={<HomePage/>}/>
         <Route path="/blog/:id" element={<ArticleDetailPage/>}/>
+        <Route path="/register" element={<Register/>}/>
       </Routes>
     </div>
   )
