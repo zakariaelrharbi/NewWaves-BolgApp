d2d6328209075092bd0a7b432dc78b6b53eff5e6
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-04-27 13:21:52 +0100

    register updated

--- Front-end/my-app/src/pages/Register/Register.jsx
+++ Front-end/my-app/src/pages/Register/Register.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { FaRegUser } from "react-icons/fa";
+import { MdMailOutline } from "react-icons/md";
+import { FaRegEye } from "react-icons/fa";
+
 
 const Register = () => {
   return (
@@ -24,23 +28,11 @@ const Register = () => {
                 className="bg-white border border-gray-300 w-full text-sm px-4 py-2.5 rounded outline-blue-500"
                 placeholder="Enter name"
               />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="#bbb"
-                stroke="#bbb"
-                className="w-4 h-4 absolute right-4"
-                viewBox="0 0 24 24"
-              >
-                <circle cx={10} cy={7} r={6} data-original="#000000" />
-                <path
-                  d="M14 15H6a5 5 0 0 0-5 5 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 5 5 0 0 0-5-5zm8-4h-2.59l.3-.29a1 1 0 0 0-1.42-1.42l-2 2a1 1 0 0 0 0 1.42l2 2a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-.3-.29H22a1 1 0 0 0 0-2z"
-                  data-original="#000000"
-                />
-              </svg>
+            <FaRegUser className="w-4 h-4 absolute right-4"/>
             </div>
           </div>
           <div>
-            <label className="text-sm mb-2 block">Email Id</label>
+            <label className="text-sm mb-2 block">Email</label>
             <div className="relative flex items-center">
               <input
                 name="email"
@@ -49,35 +41,7 @@ const Register = () => {
                 className="bg-white border border-gray-300 w-full text-sm px-4 py-2.5 rounded outline-blue-500"
                 placeholder="Enter email"
               />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="#bbb"
-                stroke="#bbb"
-                className="w-4 h-4 absolute right-4"
-                viewBox="0 0 682.667 682.667"
-              >
-                <defs>
-                  <clipPath id="a" clipPathUnits="userSpaceOnUse">
-                    <path d="M0 512h512V0H0Z" data-original="#000000" />
-                  </clipPath>
-                </defs>
-                <g
-                  clipPath="url(#a)"
-                  transform="matrix(1.33 0 0 -1.33 0 682.667)"
-                >
-                  <path
-                    fill="none"
-                    strokeMiterlimit={10}
-                    strokeWidth={40}
-                    d="M452 444H60c-22.091 0-40-17.909-40-40v-39.446l212.127-157.782c14.17-10.54 33.576-10.54 47.746 0L492 364.554V404c0 22.091-17.909 40-40 40Z"
-                    data-original="#000000"
-                  />
-                  <path
-                    d="M472 274.9V107.999c0-11.027-8.972-20-20-20H60c-11.028 0-20 8.973-20 20V274.9L0 304.652V107.999c0-33.084 26.916-60 60-60h392c33.084 0 60 26.916 60 60v196.653Z"
-                    data-original="#000000"
-                  />
-                </g>
-              </svg>
+            <MdMailOutline className="w-4 h-4 absolute right-4"/>
             </div>
           </div>
           <div>
@@ -90,18 +54,7 @@ const Register = () => {
                 className="bg-white border border-gray-300 w-full text-sm px-4 py-2.5 rounded outline-blue-500"
                 placeholder="Enter password"
               />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="#bbb"
-                stroke="#bbb"
-                className="w-4 h-4 absolute right-4 cursor-pointer"
-                viewBox="0 0 128 128"
-              >
-                <path
-                  d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
-                  data-original="#000000"
-                />
-              </svg>
+             <FaRegEye className="w-4 h-4 absolute right-4"/>
             </div>
           </div>
           <div className="flex items-center">
@@ -133,7 +86,7 @@ const Register = () => {
         <p className="text-sm mt-6 text-center">
           Already have an account?{" "}
           <a
-            href="javascript:void(0);"
+            href="/"
             className="text-blue-600 font-semibold hover:underline ml-1"
           >
             Login here
@@ -142,10 +95,6 @@ const Register = () => {
       </form>
     </div>
   </div>
-  <img
-    src="https://readymadeui.com/bg-effect.svg"
-    className="absolute inset-0 w-full h-full z-0 opacity-40"
-  />
 </div>
 
     </div>
